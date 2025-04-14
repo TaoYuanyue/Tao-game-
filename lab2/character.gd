064bd83c68e78588a9f86b7f5f90cd043e1c9e77
TaoYuanyue <ytaomica@gmail.com>
2025-04-14 13:41:43 -0400

    lab2:dash and movement

---
+++ lab2/character.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+const DASH_SPEED = 500
+var dashing = false
+var can_dash = true
+
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("dash") and can_dash :
+		dashing = true
+		can_dash = false
+		$Dash_timer.start()
+		$dash_again_timer.start()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		if dashing:
+			velocity.x = direction * DASH_SPEED
+			
+		else:
+			velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+#stop dashing
+func _on_dash_timer_timeout() -> void:
+	dashing = false
+
+
+func _on_dash_again_timer_timeout() -> void:
+	can_dash = true

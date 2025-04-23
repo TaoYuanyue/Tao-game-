feaa61660d24fe2189ab977adaee5478431d9ba2
TaoYuanyue <ytaomica@gmail.com>
2025-04-22 22:00:49 -0400

    ememy

--- capstone-game/Scripts/ememy_path.gd
+++ capstone-game/Scripts/ememy_path.gd
@@ -3,11 +3,15 @@ extends Path2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$PathFollow2D.progress_ratio += speed
+	if $PathFollow2D.progress_ratio >= 1.0 or $PathFollow2D.progress_ratio <= 0: 
+		speed = - speed
+		$PathFollow2D.flip_h = !$PathFollow2D.flip_h
+	
 	
--- capstone-game/Scripts/player.gd
+++ capstone-game/Scripts/player.gd
@@ -1,31 +1,42 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
+var SPEED = 130.0
 const JUMP_VELOCITY = -350.0
 
 const DASH_SPEED = 500
 var dashing = false
 var can_dash = true
+var Jumping = false
 
 
 
 func _physics_process(delta: float) -> void:
+	
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		Jumping = false
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("roll")
+		Jumping = true
 		
 		
 
-	if Input.is_action_just_pressed("dash") and can_dash :
+	if Input.is_action_just_pressed("dash") and can_dash and $CanvasLayer/DashBar.value >0:
 		dashing = true
 		can_dash = false
 		$Dash_Timer.start()
 		$dash_again_timer.start()
+		
+		$CanvasLayer/DashBar.value -= 100
+		$Timer.start()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -33,7 +44,9 @@ func _physics_process(delta: float) -> void:
 	if direction !=0:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.flip_h=direction <0
-		$AnimatedSprite2D.play("run")
+		if not Jumping:
+			$AnimatedSprite2D.play("run")
+
 			
 		if dashing:
 			velocity.x = direction * DASH_SPEED
@@ -41,13 +54,9 @@ func _physics_process(delta: float) -> void:
 			
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$AnimatedSprite2D.play("idel")
+		if not Jumping:
+			$AnimatedSprite2D.play("idel")
 		
-		
-		
-	
-		
-	
 		
 	
 
@@ -66,3 +75,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemys"):
 		print("You Died")
 		get_tree().reload_current_scene()
+
+
+func _on_timer_timeout() -> void:
+	$CanvasLayer/DashBar.value = 100
